440cc0a56854efc6bd4faa475ad3f8d4159720de
Fernanda Rampad <claudiafer_1997@hotmail.com>
2023-10-30 15:44:53 -0600

    Solo variables constantes

--- data.py
+++ data.py
@@ -1,18 +1,6 @@
-urban_routes_url = 'https://c1feaccd-ccf9-4333-b8e6-43a50d381fa6.serverhub.tripleten-services.com?lng=es'
+urban_routes_url = 'https://00d37f3a-3d00-4d08-98f4-7f4d44920c06.serverhub.tripleten-services.com?lng=es'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
 phone_number = '+1 123 123 12 12'
 card_number, card_code = '1234 5678 9100', '111'
 message_for_driver = 'Muéstrame el camino al museo'
-
-taxi = '//*[@id="root"]/div/div[3]/div[3]/div[1]/div[3]/div[1]/button'
-comfort = '/html/body/div/div/div[3]/div[3]/div[2]/div[1]/div[5]'
-next = '/html/body/div/div/div[1]/div[2]/div[1]/form/div[2]/button'
-
-confirm_code = '/html/body/div/div/div[1]/div[2]/div[2]/form/div[2]/button[1]'
-
-payment_code = '/html/body/div/div/div[2]/div[2]/div[2]/form/div[1]/div[2]/div[2]/div[2]/input'
-link_card = '/html/body/div/div/div[2]/div[2]/div[2]/form/div[3]/button[1]'
-close_payment = '/html/body/div/div/div[2]/div[2]/div[1]/button'
-
-counter_plus = '/html/body/div/div/div[3]/div[3]/div[2]/div[2]/div[4]/div[2]/div[3]/div/div[2]/div[1]/div/div[2]/div/div[3]'
